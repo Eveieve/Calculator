56c4915e4c9d3088c044f323223fa0091f77d4bf
Mike <nhj25252@gmail.com>
2023-02-04 20:58:06 +0900

    Add html buttons

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+//function operate should take one operator and two numbers,
+//and call one of the above functions on numbers.
+///
+//does computer know that callback is a function...?
+function operate(operator, x, y) {
+  return callback(x, y);
+}
