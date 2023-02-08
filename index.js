4540de8208824bb59cbbd079272bffbb6375d630
Mike <nhj25252@gmail.com>
2023-02-08 17:03:24 +0900

    Store pressed number

--- index.js
+++ index.js
@@ -25,24 +25,17 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
-// let numbers = [];
-
-// for (let i = 0; i < 10; i++) {
-//   const number = document.createElement("p"); //number isn't a dom yet? what?
-//   // number.textContent = `${i}`; //why don't I see this?
-//   // number.style.cssText = "display: inline";
-//   number.classList.add("num");
-//   numbers.push(number);
-// }
-
 const display = document.querySelector(".display");
 
 const btns = document.querySelectorAll(".buttons .num");
 const btnsArr = Array.from(btns);
 
+let storedNum;
+
 for (let i = 0; i < btnsArr.length; i++) {
-  btnsArr[i].addEventListener(
-    "click",
-    () => (display.textContent += btnsArr[i].textContent)
-  );
+  btnsArr[i].addEventListener("click", () => {
+    display.textContent += btnsArr[i].textContent;
+    storedNum = Number(btnsArr[i].textContent);
+    console.log(storedNum);
+  });
 }
